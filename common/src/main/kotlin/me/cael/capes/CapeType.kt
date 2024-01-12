3f11cb98c6f58d1b138efadf1a8171c82db598c2
bodaciouskrunk <48842531+bodaciouskrunk@users.noreply.github.com>
2024-01-12 05:28:57 +0000

    Update CapeType.kt

--- common/src/main/kotlin/me/cael/capes/CapeType.kt
+++ common/src/main/kotlin/me/cael/capes/CapeType.kt
@@ -5,26 +5,26 @@ import net.minecraft.screen.ScreenTexts
 import net.minecraft.text.Text
 
 enum class CapeType(val stylized: String) {
-    MINECRAFT("Minecraft"), OPTIFINE("OptiFine"), LABYMOD("LabyMod"), WYNNTILS("Wynntils"), MINECRAFTCAPES("MinecraftCapes"), COSMETICA("Cosmetica"), CLOAKSPLUS("Cloaks+");
+    MINECRAFT("Minecraft"), MINECRAFTCAPES("MinecraftCapes"), OPTIFINE("OptiFine"), LABYMOD("LabyMod"), WYNNTILS("Wynntils"), COSMETICA("Cosmetica"), CLOAKSPLUS("Cloaks+");
 
     fun cycle() = when(this) {
-        MINECRAFT -> OPTIFINE
+        MINECRAFT -> MINECRAFTCAPES
+        MINECRAFTCAPES -> OPTIFINE
         OPTIFINE -> LABYMOD
         LABYMOD -> WYNNTILS
         WYNNTILS -> COSMETICA
-        COSMETICA -> MINECRAFTCAPES
-        MINECRAFTCAPES -> CLOAKSPLUS
+        COSMETICA -> CLOAKSPLUS
         CLOAKSPLUS -> MINECRAFT
     }
 
     fun getURL(profile: GameProfile): String? {
         val config = Capes.CONFIG
         return when (this) {
+            MINECRAFTCAPES -> if(config.enableMinecraftCapesMod) "https://api.minecraftcapes.net/profile/${profile.id.toString().replace("-", "")}" else null
             OPTIFINE -> if(config.enableOptifine) "http://s.optifine.net/capes/${profile.name}.png" else null
             LABYMOD -> if(config.enableLabyMod) "https://dl.labymod.net/capes/${profile.id}" else null
             WYNNTILS -> if(config.enableWynntils) "https://athena.wynntils.com/user/getInfo" else null
             COSMETICA -> if(config.enableCosmetica) "https://api.cosmetica.cc/get/cloak?username=${profile.name}&uuid=${profile.id}&nothirdparty" else null
-            MINECRAFTCAPES -> if(config.enableMinecraftCapesMod) "https://minecraftcapes.net/profile/${profile.id.toString().replace("-", "")}" else null
             CLOAKSPLUS -> if(config.enableCloaksPlus) "http://161.35.130.99/capes/${profile.name}.png" else null
             MINECRAFT -> null
         }
