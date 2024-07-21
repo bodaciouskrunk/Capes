47852d609502558b75b3b8c9d2e9924fbd76639a
bodaciouskrunk <48842531+bodaciouskrunk@users.noreply.github.com>
2024-07-21 19:19:34 +0000

    Update CapeType.kt

--- common/src/main/kotlin/me/cael/capes/CapeType.kt
+++ common/src/main/kotlin/me/cael/capes/CapeType.kt
@@ -9,23 +9,23 @@ enum class CapeType(val stylized: String) {
 
     fun cycle() = when(this) {
         MINECRAFT -> MINECRAFTCAPES
-        MINECRAFTCAPES -> OPTIFINE
-        OPTIFINE -> LABYMOD
-        LABYMOD -> WYNNTILS
-        WYNNTILS -> COSMETICA
-        COSMETICA -> CLOAKSPLUS
-        CLOAKSPLUS -> MINECRAFT
+        MINECRAFTCAPES -> COSMETICA
+        COSMETICA -> LABYMOD
+        LABYMOD -> CLOAKSPLUS
+        CLOAKSPLUS -> OPTIFINE
+        OPTIFINE -> WYNNTILS
+        WYNNTILS -> MINECRAFT
     }
 
     fun getURL(profile: GameProfile): String? {
         val config = Capes.CONFIG
         return when (this) {
             MINECRAFTCAPES -> if(config.enableMinecraftCapesMod) "https://api.minecraftcapes.net/profile/${profile.id.toString().replace("-", "")}" else null
-            OPTIFINE -> if(config.enableOptifine) "http://s.optifine.net/capes/${profile.name}.png" else null
-            LABYMOD -> if(config.enableLabyMod) "https://dl.labymod.net/capes/${profile.id}" else null
-            WYNNTILS -> if(config.enableWynntils) "https://athena.wynntils.com/user/getInfo" else null
             COSMETICA -> if(config.enableCosmetica) "https://api.cosmetica.cc/get/cloak?username=${profile.name}&uuid=${profile.id}&nothirdparty" else null
+            LABYMOD -> if(config.enableLabyMod) "https://dl.labymod.net/capes/${profile.id}" else null
             CLOAKSPLUS -> if(config.enableCloaksPlus) "http://161.35.130.99/capes/${profile.name}.png" else null
+            OPTIFINE -> if(config.enableOptifine) "http://s.optifine.net/capes/${profile.name}.png" else null
+            WYNNTILS -> if(config.enableWynntils) "https://athena.wynntils.com/user/getInfo" else null
             MINECRAFT -> null
         }
     }
